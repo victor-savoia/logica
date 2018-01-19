cb005b3e556ba46b5a78359953b7c8d02a366f28
php 19 <php.19@penadinho.cec.ime.usp.br>
2018-01-19 19:58:14 -0200

    primeiro commit

---
+++ logica.php
@@ -0,0 +1,25 @@
+<?php
+
+//<=> igual ao compare
+
+
+
+$nota = 5;
+
+echo = "{$nota + 1}"; //concatenar o valor da variavel e depois 
+
+$mensagem;
+
+if($nota >= 5) {
+  $mensagem = "Aprovado com a nota: ";
+}else if ($nota >=3) {
+  $mensagem = "Recuperação com a nota: ";
+}  else {
+  $mensagem = "Reprovado com a nota: ";
+}
+
+echo $mensagem . $nota;
+
+echo "\n";
+
+ ?>
