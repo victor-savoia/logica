25b9d422704d47822004a55c6884108ec5370de6
php 19 <php.19@penadinho.cec.ime.usp.br>
2018-01-19 21:03:29 -0200

    varios triangulos adicionando recebimento de valor

--- variosTriangulos.php
+++ variosTriangulos.php
@@ -1,11 +1,19 @@
 <?php
 
-for ($i=0; $i <3 ; $i++) { 
+$varios = $argv[1];
+
+if(! $varios > 0 ){
+  echo "Favor enviar um numero maior que zero \n";
+  return;
+}
+
+for ($i=0; $i <$varios ; $i++) {
   for ($x=1; $x <= 5; $x++) {
       for ($y=0; $y < $x ; $y++) {
         echo '*';
       }
       echo "\n";
   }
+  echo "\n";
 }
  ?>
